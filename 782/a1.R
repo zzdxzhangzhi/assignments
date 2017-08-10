011f38d4aae0b9c7a8c209bccf3964893fa3d7d2
Zhi Zhang <zzha822@UoA.auckland.ac.nz>
2017-08-10 16:45:37 +1200

    complete first question

---
+++ 782/a1.R
@@ -0,0 +1,10 @@
+seq(1, 28, by = 3)
+paste(rep(letters[-1:-23], 3:1), rep(letters[1:3], 2), sep = "")
+rep(1:3 < 2, 2) 
+cumsum(10^(seq(0, 8, by = 2)))
+1:4 + rep(0:3, rep(4, 4))
+
+csvfile = paste(getwd(), "/assignments/782/eurocities.csv", sep = "")
+eurocities.df = read.csv(csvfile)
+str(eurocities.df)
+head(eurocities.df)
