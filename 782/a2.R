8a5b95de30d4a54ea6f6fb683b48d1456df0a0d2
Zhi Zhang <zzha822@UoA.auckland.ac.nz>
2017-08-22 16:20:59 +1200

    complete question 2 for assignment 2 of 782

--- 782/a2.R
+++ 782/a2.R
@@ -17,18 +17,32 @@ fib(3)
 fib(10)
 
 clusters.medians = function(x, c) {
-  lenx = length(x)
   lenc = length(c)
+  
   d = outer(c, x, function(cj, xi) abs(xi - cj))
-  print(d)
-  d.min = which.min(d[,1:lenx])
-  print("\n")
-  print(d.min)
-  con = outer(1:lenc, d.min, function(num, minnum) num == minnum)
-  print("\n")
-  print(con)
-  median(x[con[1:lenc,]])
+  d.minnum = apply(d, 2, which.min)
+  
+  con = outer(1:lenc, d.minnum, function(num, minnum) num == minnum)
+  
+  xv = unlist(apply(con, 1, function(t) median(x[t])))
+  xv
+}
+
+find.clusters.medians = function(x, c) {
+  ctmp1 = c
+  repeat {
+    ctmp2 = clusters.medians(x, ctmp1)
+    if (all(abs(ctmp1 - ctmp2) < 1e-07)) break
+    else ctmp1 = ctmp2
+  }
+  ctmp1
 }
 
 x = faithful$eruptions
-clusters.medians(x, c(2,4))
+find.clusters.medians(x, c(2,4))
+find.clusters.medians(x, c(2,3,4))
+find.clusters.medians(x, c(2,3,4,5))
+
+x = 8 * runif(207)
+find.clusters.medians(x, c(2,3,4,5,6))
+find.clusters.medians(x, c(2,3,4,5,6,7))
