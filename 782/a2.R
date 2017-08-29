1190c2257acea701fa00695e3452700a4e28ea04
Zhi Zhang <zzha822@UoA.auckland.ac.nz>
2017-08-29 16:52:04 +1200

    modify the answers of questions 4 of assignment 2 of 782

--- 782/a2.R
+++ 782/a2.R
@@ -74,19 +74,19 @@ head(nba.df)
 nba.names = nba.df$team1[seq(1, 870, length = 30)]
 nba.names
 
-likelihood.r = function(r, times) {
-  mtx = outer(r, r, function(ri, rj) ri / (ri + rj))
-  rankv = c(mtx[which(row(mtx) != col(mtx))]) ^ times
-  log(prod(rankv))
-}
+# likelihood.r = function(r, times) {
+#   mtx = outer(r, r, function(ri, rj) ri / (ri + rj))
+#   rankv = c(mtx[which(row(mtx) != col(mtx))]) ^ times
+#   log(prod(rankv))
+# }
 
 log.likelihood.r = function(r, times, s) {
   rn = s - sum(r)
   rr = c(r, rn)
   
   if (all(rr > 0)) {
-    mtx = outer(rr, rr, function(ri, rj) ri / (ri + rj))
-    rankv = log(c(mtx[which(row(mtx) != col(mtx))]))
+    mtx = outer(rr, rr, function(ri, rj) log(ri / (ri + rj)))
+    rankv = c(t(mtx)[which(row(mtx) != col(mtx))])
     sum(times * rankv)
   } else {
     -Inf
@@ -99,7 +99,8 @@ Q = function(r) {
 }
 
 count = length(nba.names)
-result = optim(rep(33, count - 1), Q, method = "BFGS")
+result = optim(seq(1, 29, length = 29), Q, method = "BFGS", 
+               control = list(maxit = 200))
 result
 
 ratio = 100 / result$par[which.max(result$par)]
@@ -107,47 +108,57 @@ r.value = result$par * ratio
 rr.value = c (r.value, (s - sum(result$par)) * ratio)
 rr.value
 
-rank.table = cbind(data.frame(nba.names), rr.value, stringsAsFactors = FALSE)
+rank.table = data.frame(nba.names, rr.value, stringsAsFactors = FALSE)
 ordered.rank = rank.table[order(rank.table$rr.value, decreasing = TRUE),]
 colnames(ordered.rank) = c("name", "rank")
 rownames(ordered.rank) = 1:30
 ordered.rank
 
-log.likelihood.r.deriv = function(r, times1, times2, s) {
+log.likelihood.r.deriv = function(r, i, times1, times2, s) {
+  rlen = length(r)
   rn = s - sum(r)
   rr = c(r, rn)
   
   if (all(rr > 0)) {
-    mtx1 = outer(rr, rr, function(ri, rj) rj / ri * (ri + rj))
-    mtx2 = outer(rr, rr, function(rj, ri) 1 / (ri + rj))
-    deriv1 = matrix(c(mtx1[which(row(mtx1) != col(mtx1))]) * times1, nrow = length(rr))
-    deriv2 = matrix(c(mtx2[which(row(mtx2) != col(mtx2))]) * times2, nrow = length(rr))
-    print(deriv1)
-    print(deriv2)
+    mtx1 = outer(rr[i], rr[-i], function(ri, rj) rj / ri * (ri + rj))
+    mtx2 = outer(rr[i], rr[-i], function(ri, rj) 1 / (ri + rj))
+    deriv1 = c(mtx1) * times1[(rlen * (i - 1) + 1) : (rlen * i)]
+    deriv2 = c(mtx2) * times2[(rlen * (i - 1) + 1) : (rlen * i)]
+    #print(deriv1)
+    #print(deriv2)
     
     #deriv.mt = deriv1 - deriv2
-    deriv.val = apply(deriv1, 1, sum) - apply(deriv2, 1, sum)
-    print(deriv.val)
-    deriv.val[1:length(r)]
+    deriv.val = sum(deriv1) - sum(deriv2) + 1 / r[i]
+    #deriv.val = apply(deriv1, 1, sum)
+    #print(deriv.val)
+    deriv.val
   } else {
-    -Inf
+    0
   }
 }
 
-Q.deriv = function(r) {
+Q.derivs = function(r) {
   order.team2 = order(nba.df$team2)
   wins.team2 = nba.df$wins[order.team2]
-  log.likelihood.r.deriv(r, nba.df$wins, wins.team2, s)
+  rlen = length(r)
+  gradients = numeric(rlen)
+  for (i in 1:rlen) {
+    gradients[i] = log.likelihood.r.deriv(r, i, nba.df$wins, wins.team2, s)
+  }
+  
+  gradients
 }
 
-result = optim(rep(33, count - 1), Q, gr = Q.deriv, method = "BFGS")
-result
+result.deriv = optim(rep(33, count - 1), Q, gr = Q.derivs, method = "BFGS")
+result.deriv
 
 ranks = c(result$par, s - sum(result$par))
 ranks.sort = sort(ranks, decreasing = TRUE)
 #ranks
 #ranks.sort
-first2 = which(ranks == ranks.sort[1:2])
+first2 = c(which(round(ranks) == round(ranks.sort[1])), 
+           which(round(ranks) == round(ranks.sort[2])))
+first2
 
 #fixed.val = ranks.sort[c(-1, -2)]
 #fixed.val
@@ -170,17 +181,17 @@ Q2 = function(r1, r2) {
   ans
 }
 
-r1 = seq(40, 100, length = 61)
-r2 = seq(20, 80, length = 61)
+r1 = seq(40, 145, length = 61)
+r2 = seq(20, 100, length = 61)
 z = outer(r1, r2, Q2)
 contour(r1, r2, z,
         xlab = paste("rank of", nba.names[first2[1]]),
         ylab = paste("rank of", nba.names[first2[2]]))
 
-r1 = seq(85, 100, length = 1001)
-r2 = seq(45, 75, length = 4)
+r1 = seq(40, 145, length = 1001)
+r2 = seq(20, 100, length = 6)
 z = outer(r1, r2, Q2)
-par(mfrow = c(2, 2))
+par(mfrow = c(2, 3))
 ran = range(z)
 for(j in 1:length(r2)) {
   plot(r1, z[, j], ylim = ran, type = "l",
